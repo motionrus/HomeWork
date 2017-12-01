03b76d12bd9cd46d23016dc5b323263f20304e8a
Тютин Руслан Ильдусович <r.tjutin@amtelcorp.local>
2017-12-01 11:07:41 +0300

    commit with windows git

--- lesson2/task1.py
+++ lesson2/task1.py
@@ -6,22 +6,23 @@
 # Превратите строку "01/01/17 12:10:03.234567" в объект datetime
 
 from datetime import datetime, timedelta
-import locale
+import locale, calendar
 
 DATEVIEW = '%d %B %y'
 mdays = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
 locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
-dt_now = datetime.now()
+date_now = datetime.now()
 day_delta = timedelta(days=1)
-day_in_mounth = mdays[int(dt_now.strftime('%m')) - 1]
-mounth_delta = timedelta(days=day_in_mounth)
-yestarday = dt_now - day_delta
-mounth = dt_now - mounth_delta
+days_in_mouth = mdays[int(date_now.strftime('%m')) - 1]
+mouth_delta = timedelta(days=days_in_mouth)
+yesterday = date_now - day_delta
+mouth = date_now - mouth_delta
 
-print("Дата вчерашнего дня: {}".format(yestarday.strftime(DATEVIEW)))
-print("Сегодня: {}".format(dt_now.strftime(DATEVIEW)))
-print("Месяц назад: {}".format(mounth.strftime(DATEVIEW)))
+print("Дата вчерашнего дня: {}".format(yesterday.strftime(DATEVIEW)))
+print("Сегодня: {}".format(date_now.strftime(DATEVIEW)))
+print("Месяц назад: {}".format(mouth.strftime(DATEVIEW)))
 
 str_to_obj = datetime.strptime('01/01/17 12:10:03.234567', '%d/%m/%y %H:%M:%S.%f')
+
