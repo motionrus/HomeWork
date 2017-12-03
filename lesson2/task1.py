0dbb92a0d3d6234f0b34888e2bfbdaef649b546a
rus-vista <tytin@narod.ru>
2017-12-03 18:00:35 +0300

    Задание с встречи.

--- lesson2/task1.py
+++ lesson2/task1.py
@@ -11,7 +11,7 @@ import csv
 list_stop_bus = list()
 list_street = list()
 with open('data-398-2017-12-01.csv', 'r', encoding='cp1251') as f:
-    reader = csv.DictReader(f, delimiter = ";")
+    reader = csv.DictReader(f, delimiter=";")
     for r in reader:
         list_stop_bus.append(r['StationName'])
         list_street.append(r['Street'])
@@ -26,4 +26,4 @@ print('Количество остановок в г.Москва: {}'.format(co
 print('На улице: "{}", больше всего остановок: "{}"'.format(max_count_street[0], max_count_street[1]))
 print('')
 print('Не по заданию, на в качестве бонуса.')
-print('Больше всех остановок с названием: "{}". Их количество: "{}"'.format(max_bus_stop[0],max_bus_stop[1]))
+print('Больше всех остановок с названием: "{}". Их количество: "{}"'.format(max_bus_stop[0], max_bus_stop[1]))
